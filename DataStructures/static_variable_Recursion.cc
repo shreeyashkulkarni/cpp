9eef885563af19c17f50395c76b0b1b2f276d4b3
Shreeyash Kulkarni <Shreeyashkulkarni3@gmail.com>
2022-05-25 23:32:15 +0530

    Added static_variable_Recursion.cc

---
+++ DataStructures/static_variable_Recursion.cc
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int fun(int n)
+{
+	static int x =0;
+
+	if(n>0)
+	{
+		x++;
+		return fun(n-1) + x;
+	}
+	return 0;
+}
+
+int main()
+{
+	int r;
+	r = fun(5);
+	cout << r << endl;
+
+
+    int p;
+	p = fun(5);
+	cout << p << endl;
+
+
+	return 0;
+}
